8a2bbb6c424d1e8066fa723be1b3c066b331e406
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2024-09-08 20:46:57 +0530

    shifting cookie based authentication to Local Storage {1}

--- src/api/axios_instance.js
+++ src/api/axios_instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: 'http://localhost:2006/api/',
+  baseURL: 'https://todo-server-ikof.onrender.com/api/',
   headers: {
     "Content-Type": "application/json",
   },
